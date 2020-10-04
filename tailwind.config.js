6c0e9ecdf176401c875c9655bd099e336950dfd0
Duncan McClean <duncan@mcclean.co.uk>
2020-10-04 18:04:49 +0100

    Use single quotes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+  purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
   theme: {
     extend: {
       colors: {
-        "hacktoberfest-blue": "#0069ff",
-        "hacktoberfest-orange": "#9c4668",
+        'hacktoberfest-blue': '#0069ff',
+        'hacktoberfest-orange': '#9c4668"',
       },
     },
   },
